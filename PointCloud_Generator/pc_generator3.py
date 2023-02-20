71a740ec5a97b913af505f3f5ec0586c7a0f9e19
Jarrypho <62235645+Jarrypho@users.noreply.github.com>
2023-02-20 09:49:57 +0100

    Update pc_generator3.py

--- PointCloud_Generator/pc_generator3.py
+++ PointCloud_Generator/pc_generator3.py
@@ -1,3 +1,20 @@
+'''
+Copyright (C) 2022  Jan-Philipp Kaiser
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+'''
+
 # Contribution: This code is based on https://github.com/mikedh/trimesh/issues/620
 import trimesh
 import numpy as np
